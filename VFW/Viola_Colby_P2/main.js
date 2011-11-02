64b7232087decad75ca9f35cfa84aaa12af65320
Colby <colbyviola@mail.com>
2011-11-01 22:06:08 -0700

    Added makeCats function.

--- VFW/Viola_Colby_P2/main.js
+++ VFW/Viola_Colby_P2/main.js
@@ -5,13 +5,32 @@
 // Full Sail University
 
 window.addEventlistener("DOMContentLoaded", function(){
+	//getElementById Function
 	function $(x){
 		var theElement = document.getElementById(x);
 		return theElement;
 	}
-
+	
+	//Create select field element and populate with options.
+	function makeCats(){
+		var formTag = document.getElementsByTagName("form"), //formTag is an array of all the form tags.
+			selectLi = $("select"),
+			makeSelect = document.createElement("select");
+			makeSelect.setAttribute("id", "groups"); //makeSelect has an "id" of groups
+		for(var i=0, j=contactGroups.length; i<j; i++){
+			var makeOption = document.createElement ("option");
+			var optText = movieList[i];
+			makeOption.setAttribute("value", optText); // setAttribute has a "value" of optText
+			makeOption.innerHTML = optText;
+			makeSelect.appendChild(makeOption);
+		}
+		selectLi.appendChild(makeSelect);
+	}
+	//Variable defaults
 	var movieList = ["--Choose A Group--", "DVD", "Theaters", "Unknown"];
+	makeCats();
 
+//Set link & submit click events
 	var displayLink = $("displayLink");
 	displayLink.addEventListener("click", getData);
 	var clearLink = $("clear");
