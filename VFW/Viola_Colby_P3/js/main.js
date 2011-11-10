ad91b90f8e3c6d4fb806f160ee73cf2c26242684
Colby <colbyviola@mail.com>
2011-11-09 19:17:52 -0800

    Revert "Corrected self closing syntax error."
    This reverts commit e8d66f05f47fb3505064f54f0082bcc2678ce0cc.

--- VFW/Viola_Colby_P3/js/main.js
+++ VFW/Viola_Colby_P3/js/main.js
@@ -66,16 +66,8 @@ window.addEventListener("DOMContentLoaded", function(){
 		}
 	}
 	
-	function storeData(key){
-		//If there is no key, this means item is brand new, and new key is needed.
-		if(!key){
-			var id			= Math.floor(Math.random()*100001);	
-		}else{
-			//Set the id to the existing key we're editing so that it will save over the data.
-			//Same key that has been passed through edit submit handler,
-			//to the validate function, then passed here, into storeData function
-			id = key;
-		}
+	function storeData(){
+		var id			= Math.floor(Math.random()*100001);
 		//Gather up all our form field values and store in an object.
 		//Object properties contain array with the form label and input value.
 		getSelectedRadio();
@@ -123,7 +115,7 @@ window.addEventListener("DOMContentLoaded", function(){
 				makeSubli.innerHTML = optSubText;
 				makeSubList.appendChild(linksLi);
 			}
-			makeItemLinks(localStorage.key(i), linksLi); //Creates edit and delete buttons/links for each item in local storage.
+			makeItemLinks(localStorage.key(i), linksLi)); //Creates edit and delete buttons/links for each item in local storage.
 		}
 	}
 	//Make Item Links
@@ -152,60 +144,6 @@ window.addEventListener("DOMContentLoaded", function(){
 		linksLi.appendChild(deleteLink);
 	}
 	
-	function editItem(){
-		//Grab the data from our item in local storage.
-		var value = localStorage.getItem(this.key);
-		var item = JSON.parse(value);
-		
-		//shows the form
-		toggleControls("off");
-		
-		//populate the form fields with current localStorage values.
-		$("groups").value = item.group[1];
-		$("fname").value = item.fname[1];
-		$("rdate").value = item.rdate[1];
-		var radios = document.forms[0].pview;
-		for(var i=0; i<radios.length; i++){
-			if (radios[i].value == "Comedy" && item.genre[1] == "Comedy"){
-				radios[i].setAttribute("checked", "checked");
-			}else if(radios[i].value == "Drama" && item.genre[1] == ("Drama"){
-				radios[i].setAttribute("checked", "checked");
-				}
-			}else if(radios[i].value == "Action" && item.genre[1] == ("Action"){
-				radios[i].setAttribute("checked", "checked");
-				}
-			}else if(radios[i].value == "Thriller" && item.genre[1] == ("Thriller"){
-				radios[i].setAttribute("checked", "checked");
-				}
-			}else if(radios[i].value == "Romance" && item.genre[1] == ("Romance"){
-				radios[i].setAttribute("checked", "checked");
-				}
-			}else if(radios[i].value == "Horror" && item.genre[1] == ("Horror"){
-				radios[i].setAttribute("checked", "checked");
-				}
-			}else if(radios[i].value == "Documentary" && item.genre[1] == ("Documentary"){
-				radios[i].setAttribute("checked", "checked");
-				}
-			}
-		if(item.pview[1] == "Yes"){
-			$("pview").setAttribute("checked", "checked");
-		}
-		$("range").value = item.range[1];
-		$("tlink").value = item.tlink[1];
-		$("comments").value = item.comments[1];
-			
-		//Remove the initial listener from the input "save movie" button.
-		save.removeEventListener("click", storeData);
-		//Change submit button value to say edit button.
-		$("submit").value = "Edit Movie";
-		var editSubmit = $("submit");
-		//Save the key value established in this function as a property of the editSubmit event
-		//so we can use that value when edited data is saved.
-		editSubmit.addEventListener("click", validate);
-		editSubmit.key = this.key;
-	}
-	
-	
 	function clearLocal(){
 		if(localStorage.length === 0){
 			alert("There is no data to clear.")
@@ -216,55 +154,11 @@ window.addEventListener("DOMContentLoaded", function(){
 			return false;
 		}
 	}
-	//e stands for event data
-	function validate(e){
-		//Define the elements we want to check.
-		var getGroup = $("groups");
-		var getFname = $("fname");
-		
-		//Reset Error messages.
-		errMsg.innerHTML = "";
-		getGroup.style.border = "1px solid black";
-		getFname.style.border = "1px solid black";
-		
-		//Get error messages.
-		var messageAry = [];
-		//Group validation
-		if(getGroup.value === "--Choose A Group--")
-			var groupError = "Please choose a group.";
-			getGroup.style.border = "1px solid red";
-			messageAry.push(groupError);
-		}
-		
-		//Film name validation
-		if(getFname.value === ""){
-			var fNameError = "Please enter a film name."
-			getFname.style.border = "1px solid red";
-			messageAry.push(fNameError);
-		}
-		
-		//If there were errors, display them on the screen.
-		if(messageAry.length >= 1){
-			for(var i=0, j=messageAry.length; i < j; i++){
-				var txt = document.createElement("li");
-				txt.innerHTML = messageAry[i];
-				errMsg.appendChild(txt);
-			}
-			e.preventDefault();
-			return false;
-		}else{
-			//If all is OK, save our data. Send the key value (orginated from editData function).
-			//Remember this key value was passed through editSubmit event listener as a property.
-			storeData(this.key);
-		}
-		
-	}
 	
 	//Variable defaults
 	var movieGroup = ["--Choose A Group--", "DVD", "Theaters", "Unknown"],
 		genreValue,
-		pviewValue = "No"
-		errMsg = $("errors")
+		pviewValue
 	;
 	makeCats();
 
@@ -275,7 +169,7 @@ window.addEventListener("DOMContentLoaded", function(){
 	var clearLink = $("clear");
 	clearLink.addEventListener("click", clearLocal);
 	var save = $("submit");
-	save.addEventListener("click", validate);
+	save.addEventListener("click", storeData);
 	
 
 });
