1462fcdf77edeba3c604d1bb61e89247d79624ed
Colby <colbyviola@mail.com>
2011-12-07 21:34:27 -0800

    Revert "Revert 0dbd7a3849cb773e43b56c1a2e1eaf570a8f7ff5^..HEAD"
    This reverts commit be502ed6728c03766a45c5b64549b7989ca2d6e6.

--- MIU/Viola_Colby_P3Good/Good App/js/main.js
+++ MIU/Viola_Colby_P3Good/Good App/js/main.js
@@ -10,12 +10,24 @@ var parseMovieForm = function (data){
 
 $(document).ready(function(){
 	
-	var rbform = $("#addmovieform");
+	var amform = $("#addmovieform"),
+		amerrorslink = $("amerrorslink")
+	;
 	
-	rbform.validate({
-		invalidHandler: function(form, validator){},
+	amform.validate({
+		invalidHandler: function(form, validator){
+			amerrorslink.click();
+			var html ="";
+			for(var key in validator.submitted){
+				var label = $('label[for^="'+ key +'"]').not("[generated]");
+				var legend = label.closest("fieldset").find(".ui-controlgroup-label");
+				var fieldName= legend.length ? legend.text() : label.text();
+				html += "<li>"+ fieldName +"</li>";
+			};
+			$("#addmovieerrors ul").html(html);
+		},
 		submitHandler: function(){
-			var data = rbform.serializeArray();
+			var data = amform.serializeArray();
 			parseMovieForm(data);
 		}
 	});
