d9e70f251d9d5cbe6153868dbad647969f3a6fda
Colby <colbyviola@mail.com>
2011-12-14 19:35:30 -0800

    Created P4 Good file.

---
+++ MIU/Viola_Colby_P4Good /Good App/js/main.js
@@ -0,0 +1,34 @@
+// Colby Viola
+// Project 3
+// Mobile Interfaces and Usability (MIU)
+// Mobile Development
+// Full Sail University
+var parseMovieForm = function (data){
+	// uses form data here;
+	console.log(data);
+};
+
+$(document).ready(function(){
+	
+	var amform = $("#addmovieform"),
+		amerrorslink = $("amerrorslink")
+	;
+	
+	amform.validate({
+		invalidHandler: function(form, validator){
+			amerrorslink.click();
+			var html ="";
+			for(var key in validator.submitted){
+				var label = $('label[for^="'+ key +'"]').not("[generated]");
+				var legend = label.closest("fieldset").find(".ui-controlgroup-label");
+				var fieldName= legend.length ? legend.text() : label.text();
+				html += "<li>"+ fieldName +"</li>";
+			};
+			$("#addmovieerrors ul").html(html);
+		},
+		submitHandler: function(){
+			var data = amform.serializeArray();
+			parseMovieForm(data);
+		}
+	});
+});
